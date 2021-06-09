110ab891da42e44a4b07930525b821f275159f8b
Brian Lemons <brian_lemons@hotmail.com>
2021-06-08 22:10:44 -0700

    adding some basic tests

--- tests/test_web_scraper.py
+++ tests/test_web_scraper.py
@@ -1,6 +1,10 @@
 from web_scraper import __version__
-from web_scraper.scraper import get_citations_needed_count
+from web_scraper.scraper import get_citations_needed_count, get_citations_needed_report
 
 
 def test_url_working():
     assert get_citations_needed_count
+
+def test_other_url():
+    assert get_citations_needed_report
+
