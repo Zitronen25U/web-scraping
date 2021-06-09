5df49852cb355101cfc6b05d5bf8ace1aca8aab7
Brian Lemons <brian_lemons@hotmail.com>
2021-06-08 22:07:58 -0700

    adding some stuff I think is right, probably not

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -7,8 +7,14 @@ def get_citations_needed_count(URL):
     citations_needed = soup.find_all("a", title="Wikipedia:Citation needed")
     return len(citations_needed)
 
-def get_citations_needed_report():
-    pass
+def get_citations_needed_report(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    data = soup.find_all("a", title="Wikipedia:Citation needed")
+    print(f"The amount of citations needed is {data}")
 
+
+string_data = get_citations_needed_report("https://en.wikipedia.org/wiki/Agios_Epiktitos")
 total_count = get_citations_needed_count("https://en.wikipedia.org/wiki/Agios_Epiktitos")
 print(total_count)
+
